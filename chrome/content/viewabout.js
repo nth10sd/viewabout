482bd9332813ff7222dba5f90e8f3f12b1c84aa5
Mark Banner (Standard8) <Mark Banner (Standard8)@localhost>
2010-04-04 22:26:50 +0800

    Add suggested changes to make ViewAbout open tabs in Thunderbird instead of windows.

--- chrome/content/viewabout.js
+++ chrome/content/viewabout.js
@@ -20,9 +20,44 @@ var viewabout =
         }
     },
 
-    openDialog : function(aboutURL)
+    openUrl : function(aboutURL)
     {
-        var filename = aboutURL.replace(":","").replace("?","").replace("=","");
+      var filename = aboutURL.replace(":","").replace("?","").replace("=","");
+      var versionCheck = Components.classes["@mozilla.org/xpcom/version-comparator;1"]
+                                   .getService(Components.interfaces.nsIVersionComparator);
+
+      if (Application.id == "{3550f703-e582-4d05-9a08-453d09bdfdc6}" &&
+          versionCheck.compare(Application.version, "3.0") >= 0) {
+
+        // This is Thunderbird
+        let tabmail = document.getElementById("tabmail");
+        if (!tabmail) {
+          // Try opening new tabs in an existing 3pane window
+          let mail3PaneWindow =
+            Components.classes["@mozilla.org/appshell/window-mediator;1"]
+                      .getService(Components.interfaces.nsIWindowMediator)
+                      .getMostRecentWindow("mail:3pane");
+          if (mail3PaneWindow) {
+            tabmail = mail3PaneWindow.document.getElementById("tabmail");
+            mail3PaneWindow.focus();
+          }
+        }
+
+        const tabParams = {
+          contentPage: aboutURL,
+          clickHandler: "specialTabs.aboutClickHandler(event);"
+        };
+
+        if (tabmail)
+          tabmail.openTab("contentTab", tabParams);
+        else
+          window.openDialog("chrome://messenger/content/", "_blank",
+                            "chrome,dialog=no,all", null,
+                            { tabType: "contentTab",
+                              tabParams: tabParams });
+      }
+      else
+        // This is SeaMonkey/Firefox etc
         window.openDialog("chrome://viewabout/content/dialogs/"+filename+".xul", filename, "chrome,resizable=yes,width=800,height=500,centerscreen");
     }
 };
